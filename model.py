c44b484d08037c3a15f8f5ca980c3581603c87ec
solus9 <erarungwl2013@gmail.com>
2019-11-12 22:06:40 +0530

    efdfd

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class AgentNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, h1_size = 256, h2_size = 256):
+    def __init__(self, state_size, action_size, seed, h1_size = 16, h2_size = 16):
         """Initialize parameters and build model.
         Params
         ======
@@ -17,12 +17,12 @@ class AgentNetwork(nn.Module):
         self.seed = torch.manual_seed(seed)
         "*** YOUR CODE HERE ***"
         self.fc1 = nn.Linear(state_size, h1_size)
-        self.fc2 = nn.Linear(h1_size, h2_size)
+        #self.fc2 = nn.Linear(h1_size, h2_size)
         self.fc3 = nn.Linear(h2_size, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action probabilities."""
         x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
+        #x = F.relu(self.fc2(x))
         x = F.softmax(self.fc3(x))
         return x
